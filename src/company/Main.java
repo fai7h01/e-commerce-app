7a9ae56138f6b1a9843b7cb46651a275940ddeee
fai7h01 <l.varsimashvil10@gmail.com>
2024-02-19 22:21:22 +0400

    created objects in main method

---
+++ src/company/Main.java
@@ -0,0 +1,14 @@
+package company;
+
+import static company.DataGenerator.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        createCustomer();
+        createCategory();
+        createProduct();
+
+
+    }
+}
