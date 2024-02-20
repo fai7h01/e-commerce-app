7c0cc2bfd9ae6f47b0298d9243334899816f1537
fai7h01 <l.varsimashvil10@gmail.com>
2024-02-21 00:19:57 +0400

    case 2 case 3 and case 4 added to main

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,8 +1,13 @@
 package company;
 
+import company.balance.Balance;
+import company.balance.CustomerBalance;
+import company.balance.GiftCardBalance;
 import company.category.Category;
+import company.discount.Discount;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 import static company.DataGenerator.*;
 import static company.StaticConstants.*;
@@ -53,10 +58,38 @@ public class Main {
                     }
                     break;
                 case 2: // list discounts
+                    for (Discount discount : DISCOUNT_LIST) {
+                        System.out.println("Discount name: " + discount.getName() + " discount threshold amount: " + discount.getThresholdAmount());
+                    }
                     break;
                 case 3: // see balance
+                    CustomerBalance cBalance = findCustomerBalance(customer.getId());
+                    GiftCardBalance gBalance = findGiftCardBalance(customer.getId());
+                    double totalBalance = cBalance.getBalance() + gBalance.getBalance();
+                    System.out.println("Total Balance: " + totalBalance);
+                    System.out.println("Customer balance: " + cBalance.getBalance());
+                    System.out.println("Gift card balance: " + gBalance.getBalance());
                     break;
                 case 4: // add balance
+                    CustomerBalance customerBalance = findCustomerBalance(customer.getId());
+                    GiftCardBalance giftCardBalance = findGiftCardBalance(customer.getId());
+                    System.out.println("Which account would you like to add?");
+                    System.out.println("Type 1 for customer balance: " + customerBalance.getCustomerId());
+                    System.out.println("Type 2 for gift card balance: " + giftCardBalance.getBalance());
+                    int balanceAccountSelection = scanner.nextInt();
+                    System.out.println("How much would you like to add?");
+                    double amount = scanner.nextDouble();
+                    switch (balanceAccountSelection){
+                        case 1:
+                            customerBalance.addBalance(amount);
+                            System.out.println("New customer balance: " + customerBalance.getBalance());
+                            break;
+                        case 2:
+                            giftCardBalance.addBalance(amount);
+                            System.out.println("New gift card balance: " + giftCardBalance.getBalance());
+                            break;
+                    }
+
                     break;
                 case 5: // place an order
                     break;
@@ -74,6 +107,31 @@ public class Main {
 
 
 
+    }
+
+    private static CustomerBalance findCustomerBalance(UUID customerId){
+        for (Balance balance : CUSTOMER_BALANCE_LIST) {
+            if (balance.getCustomerId().toString().equals(customerId.toString())){
+                return (CustomerBalance) balance;
+            }
+        }
+        CustomerBalance customerBalance = new CustomerBalance(customerId, 0d);
+        CUSTOMER_BALANCE_LIST.add(customerBalance);
+
+        return customerBalance;
+    }
+
+    private static GiftCardBalance findGiftCardBalance(UUID customerId){
+        for (Balance balance : GIFT_CARD_BALANCE_LIST) {
+            if (balance.getCustomerId().toString().equals(customerId.toString())){
+                return (GiftCardBalance) balance;
+            }
+        }
+
+        GiftCardBalance giftCardBalance = new GiftCardBalance(customerId, 0d);
+        GIFT_CARD_BALANCE_LIST.add(giftCardBalance);
+
+        return giftCardBalance;
     }
 
     private static String[] prepareMenuOptions(){
